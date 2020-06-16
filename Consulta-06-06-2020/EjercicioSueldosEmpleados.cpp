f793e19e24e28ba70f37172d3b89ab227da1ae5f
chelo154 <chelo154@gmail.com>
2020-06-16 01:25:40 -0300

    Update ejercicios de Consulta

--- Consulta-06-06-2020/EjercicioSueldosEmpleados.cpp
+++ Consulta-06-06-2020/EjercicioSueldosEmpleados.cpp
@@ -1,27 +1,36 @@
 #include <stdio.h>
 
-//Definición de la función
+//Definiciï¿½n de la funciï¿½n
 void calcular_sueldo(int cantidad_horas_trabajadas,float precio_hora, float &sueldo_calculado);
 
 
 main(){
-	//Declaración de variables
-	int cantidad_horas_trabajadas;
+	//Declaraciï¿½n de variables
+	int cantidad_horas_trabajadas;	
 	float precio_hora,concepto_total_sueldos=0,sueldo=0;
 
 	//Pedir los datos
 	printf("Ingrese la cantidad de horas trabajadas: ");
 	scanf("%d",&cantidad_horas_trabajadas);
-	while(cantidad_horas_trabajadas > 0){
-		printf("Ingrese el precio de la hora del empleado: ");
-		scanf("%f",&precio_hora);
-		//Llamado a la función
+
+	printf("Ingrese el precio de la hora del empleado: ");
+	scanf("%f",&precio_hora);
+
+	while(precio_hora > 0){
+		
+		//Llamado a la funciï¿½n
 		calcular_sueldo(cantidad_horas_trabajadas,precio_hora,sueldo);
 		concepto_total_sueldos += sueldo;
+
 		printf("\nSueldo a pagar del empleado: %.2f",sueldo);
+
 		printf("\nIngrese la cantidad de horas trabajadas: ");
 		scanf("%d",&cantidad_horas_trabajadas);
+
+		printf("Ingrese el precio de la hora del empleado: ");
+		scanf("%f",&precio_hora);
 	}
+
 	printf("Concepto total en sueldos: %.2f",concepto_total_sueldos);
 	getchar();
 	getchar();
@@ -30,20 +39,28 @@ main(){
 
 //Funcion sin tipo
 void calcular_sueldo(int cantidad_horas_trabajadas,float precio_hora, float &sueldo_calculado){
-	//declaración de variables	
+	//declaraciï¿½n de variables	
 	const float descuento_jubilacion = 11;
 	const float descuento_obra_social = 3;
 	const float premio_horas_trabajadas = 5;
+
 	float valor_descuento_jubilacion,valor_descuento_obra_social,valor_premio_horas_trabajadas,sueldo_neto;
+
 	sueldo_neto = (float)cantidad_horas_trabajadas * precio_hora;
+
 	valor_descuento_jubilacion = sueldo_neto *(descuento_jubilacion / 100);
+
 	valor_descuento_obra_social = sueldo_neto * (descuento_obra_social / 100);
-	if(cantidad_horas_trabajadas >= 60){
+
+	if(cantidad_horas_trabajadas >= 60)
+	{
 		valor_premio_horas_trabajadas = sueldo_neto * (premio_horas_trabajadas /  100);	
 	} 
-	else{
+	else
+	{
 		valor_premio_horas_trabajadas = 0;	
 	}	
+	
 	sueldo_calculado = sueldo_neto - valor_descuento_jubilacion - valor_descuento_obra_social + valor_premio_horas_trabajadas;
 }
 
